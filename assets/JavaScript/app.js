45e663108c8f371ede58885c8a3a0653ebad957f
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-08-21 20:21:23 +0100

    Added some of the round, score and deduction variables in the JS file

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,7 +1,22 @@
-let round1ScoreRed = document.getElementById("round_1_score_red")
-let round1DeductionsRed = document.getElementById("round_1_deductions_red")
-let round1ScoreBlue = document.getElementById("round_1_score_blue")
-let round1DeductionsBlue = document.getElementById("round_1_deductions_blue")
+const round1ScoreRed = document.getElementById("round_1_score_red")
+const round1DeductionsRed = document.getElementById("round_1_deductions_red")
+const round1ScoreBlue = document.getElementById("round_1_score_blue")
+const round1DeductionsBlue = document.getElementById("round_1_deductions_blue")
+
+const round2ScoreRed = document.getElementById("round_2_score_red")
+const round2DeductionsRed = document.getElementById("round_2_deductions_red")
+const round2ScoreBlue = document.getElementById("round_2_score_blue")
+const round2DeductionsBlue = document.getElementById("round_2_deductions_blue")
+
+const round3ScoreRed = document.getElementById("round_3_score_red")
+const round3DeductionsRed = document.getElementById("round_3_deductions_red")
+const round3ScoreBlue = document.getElementById("round_3_score_blue")
+const round3DeductionsBlue = document.getElementById("round_3_deductions_blue")
+
+const round4ScoreRed = document.getElementById("round_4_score_red")
+const round4DeductionsRed = document.getElementById("round_4_deductions_red")
+const round4ScoreBlue = document.getElementById("round_4_score_blue")
+const round4DeductionsBlue = document.getElementById("round_4_deductions_blue")
 
 let totalScoreRed = document.getElementById("total_score_red") //total score for red team
 let totalScoreBlue = document.getElementById("total_score_blue") //total score for blue team
