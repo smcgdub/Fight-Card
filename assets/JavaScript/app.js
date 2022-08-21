b7738eddda5947f9b5219cf656789105c85c78e2
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-08-21 20:32:28 +0100

    Added the JS variables

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -18,11 +18,59 @@ const round4DeductionsRed = document.getElementById("round_4_deductions_red")
 const round4ScoreBlue = document.getElementById("round_4_score_blue")
 const round4DeductionsBlue = document.getElementById("round_4_deductions_blue")
 
-let totalScoreRed = document.getElementById("total_score_red") //total score for red team
-let totalScoreBlue = document.getElementById("total_score_blue") //total score for blue team
+const round5ScoreRed = document.getElementById("round_5_score_red")
+const round5DeductionsRed = document.getElementById("round_5_deductions_red")
+const round5ScoreBlue = document.getElementById("round_5_score_blue")
+const round5DeductionsBlue = document.getElementById("round_5_deductions_blue")
 
+const round6ScoreRed = document.getElementById("round_6_score_red")
+const round6DeductionsRed = document.getElementById("round_6_deductions_red")
+const round6ScoreBlue = document.getElementById("round_6_score_blue")
+const round6DeductionsBlue = document.getElementById("round_6_deductions_blue")
+
+const round7ScoreRed = document.getElementById("round_7_score_red")
+const round7DeductionsRed = document.getElementById("round_7_deductions_red")
+const round7ScoreBlue = document.getElementById("round_7_score_blue")
+const round7DeductionsBlue = document.getElementById("round_7_deductions_blue")
+
+const round8ScoreRed = document.getElementById("round_8_score_red")
+const round8DeductionsRed = document.getElementById("round_8_deductions_red")
+const round8ScoreBlue = document.getElementById("round_8_score_blue")
+const round8DeductionsBlue = document.getElementById("round_8_deductions_blue")
+
+const round9ScoreRed = document.getElementById("round_9_score_red")
+const round9DeductionsRed = document.getElementById("round_9_deductions_red")
+const round9ScoreBlue = document.getElementById("round_9_score_blue")
+const round9DeductionsBlue = document.getElementById("round_9_deductions_blue")
+
+const round10ScoreRed = document.getElementById("round_10_score_red")
+const round10DeductionsRed = document.getElementById("round_10_deductions_red")
+const round10ScoreBlue = document.getElementById("round_10_score_blue")
+const round10DeductionsBlue = document.getElementById("round_10_deductions_blue")
+
+const round11ScoreRed = document.getElementById("round_11_score_red")
+const round11DeductionsRed = document.getElementById("round_11_deductions_red")
+const round11ScoreBlue = document.getElementById("round_11_score_blue")
+const round11DeductionsBlue = document.getElementById("round_11_deductions_blue")
+
+const round12ScoreRed = document.getElementById("round_12_score_red")
+const round12DeductionsRed = document.getElementById("round_12_deductions_red")
+const round12ScoreBlue = document.getElementById("round_12_score_blue")
+const round12DeductionsBlue = document.getElementById("round_12_deductions_blue")
+
+const totalScoreRedUI = document.getElementById("total_score_red") //total score for red boxer
+const totalScoreBlueUI = document.getElementById("total_score_blue") //total score for blue boxer
+const totalDeductionsRedUI = document.getElementById("total_deductions_red") //total deductions for red boxer
+const totalDeductionsBlueUI = document.getElementById("total_deductions_blue") //total deductions for blue boxer
+
+// Calculate scores function
 function calculateScores() {
   let round1RedTotal = round1ScoreRed - round1DeductionsRed
   let round1BlueTotal = round1ScoreBlue - round1DeductionsBlue
   let totalScoreRedDisplay = round1ScoreRed - round1DeductionsRed
+}
+
+// Reset scores function
+function resetScorecard() {
+
 }
